224504dd9a8fb9b9a4d7812fa717c02cf349d7ac
Tim Southcombe <tim.southcombe@gmail.com>
2021-02-13 17:26:38 +0000

    Added in a little the display of messages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Paper, InputBase, Divider, IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import SendIcon from '@material-ui/icons/Send';
+import { 
+  Container,
+  Paper, 
+  InputBase,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText
+} from '@material-ui/core';
 
 const App = () => {
   const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
   const [socket, setSocket] = useState(false);
   
   useEffect(() => {
     const socket = new WebSocket('wss://buster:3030');
     
     socket.addEventListener('message', (e) => {
-      console.log(e.data);
+      setMessages((a) => [e.data, ...a]);
     });
     
     socket.addEventListener('close', (e) => {
@@ -39,6 +49,16 @@ const App = () => {
       height: 28,
       margin: 4,
     },
+    messages: {
+      margin: '20px',
+      padding: '2px 4px',
+      display: 'flex',
+      alignItems: 'center',
+      width: 400,
+      position: 'relative',
+      overflow: 'auto',
+      maxHeight: 500,
+    }
   }));
   const classes = useStyles();
   const sendMesage = (e) => {
@@ -60,6 +80,20 @@ const App = () => {
         <IconButton type="submit" className={classes.iconButton}>
           <SendIcon />
         </IconButton>
+      </Paper>
+      <Paper className={classes.messages} >
+        <List>
+          {messages.map((m, k) => 
+            <ListItem key={k}>
+              <ListItemText primary={m} />
+            </ListItem>
+          )}
+          {messages.length === 0 && (
+            <ListItem>
+              <ListItemText secondary="The chat messages will be shown here" />
+            </ListItem>
+          )}
+        </List>
       </Paper>
     </Container>
   );
